6ab16131a4d857eee6e36f6518e48223b19ac010
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-06-12 10:50:49 -0700

    adds heroku production backend

--- utils/axios-instance.js
+++ utils/axios-instance.js
@@ -4,7 +4,7 @@ export const axiosInstance = () => {
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
-    : `https://<production_link>/api`
+    : `${process.env.PRODUCTION_API_URL}/api`
   })
 }
 
@@ -12,7 +12,7 @@ export const axiosWithAuth = () => {
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
-    : `https://<production_link>/api`,
+    : `${process.env.PRODUCTION_API_URL}/api`,
     withCredentials: true
   })
 }
