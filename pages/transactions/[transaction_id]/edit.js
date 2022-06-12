cccb9944db33d3a826589d9087b3fe24b36f428b
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-06-12 11:04:46 -0700

    removes undefined code

--- pages/transactions/[transaction_id]/edit.js
+++ pages/transactions/[transaction_id]/edit.js
@@ -98,7 +98,7 @@ export default function EditTransactionPage() {
       button={{
         back: {
           text: 'Back',
-          href: `/transactions/${params.transaction_id}`
+          // href: `/transactions/${params.transaction_id}`
         }
       }}
       helperText={helperText}
