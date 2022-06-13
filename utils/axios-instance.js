91f8c2db6e92a37d8a9879f9653797f340895327
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-06-13 11:16:44 -0700

    updated env with NEXT_PUBLIC prefix

--- utils/axios-instance.js
+++ utils/axios-instance.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export const axiosInstance = () => {
-  console.log(process.env.REACT_APP_PRODUCTION_API_URL)
+  console.log(process.env.NEXT_PUBLIC_PRODUCTION_API_URL)
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
-    : process.env.REACT_APP_PRODUCTION_API_URL
+    : process.env.NEXT_PUBLIC_PRODUCTION_API_URL
   })
 }
 
 export const axiosWithAuth = () => {
-  console.log(process.env.REACT_APP_PRODUCTION_API_URL)
+  console.log(process.env.NEXT_PUBLIC_PRODUCTION_API_URL)
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
-    : process.env.REACT_APP_PRODUCTION_API_URL,
+    : process.env.NEXT_PUBLIC_PRODUCTION_API_URL,
     withCredentials: true
   })
 }
