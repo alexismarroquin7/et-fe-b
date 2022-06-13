62b9cbed4f498a65f25cd4fbdf15a53cd315c230
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-06-13 11:11:26 -0700

    test

--- utils/axios-instance.js
+++ utils/axios-instance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = () => {
+  console.log(process.env.REACT_APP_PRODUCTION_API_URL)
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
@@ -9,6 +10,7 @@ export const axiosInstance = () => {
 }
 
 export const axiosWithAuth = () => {
+  console.log(process.env.REACT_APP_PRODUCTION_API_URL)
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
