87f345fd2aff634e1fbea3d1b010a3a8cfe11f64
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-06-13 11:22:28 -0700

    removes unneeded console.log

--- utils/axios-instance.js
+++ utils/axios-instance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = () => {
-  console.log(process.env.NEXT_PUBLIC_PRODUCTION_API_URL)
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
@@ -10,7 +9,6 @@ export const axiosInstance = () => {
 }
 
 export const axiosWithAuth = () => {
-  console.log(process.env.NEXT_PUBLIC_PRODUCTION_API_URL)
   return axios.create({
     baseURL: process.env.NODE_ENV === 'development'
     ? `http://localhost:4000/api`
